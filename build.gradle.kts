be84957859f681cbdac325b858dc3ec4239f0e81
bis2be <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>
2022-08-18 20:51:49 +0200

    feat: add gradle io.ktor.plugin to make fat jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,7 @@ plugins {
     application
     kotlin("jvm") version "1.7.10"
     kotlin("plugin.serialization") version "1.7.10"
+    id("io.ktor.plugin") version "2.1.0"
 }
 
 group = "wf.poc.ktor2"
